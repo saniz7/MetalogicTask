93d69e2f20e6a460db512a970a393bfb982e5432
Sanij <Sanizsanij@gmail.com>
2024-04-16 21:45:32 +0545

    fix[countup]:countup animation

--- src/components/Countup.tsx
+++ src/components/Countup.tsx
@@ -1,14 +1,34 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+
 const Countup = () => {
+    const [startCount, setStartCount] = useState(0);
+    const { ref, inView } = useInView();
+
+    // Trigger count when entering viewport
+    useEffect(() => {
+        if (inView) {
+            setStartCount(0);
+            console.log('view');
+            
+        }
+    }, [inView]);
+
     return (
         <div className='container w-full translate-y-12 z-50'>
-            <div className='mx-4 flex flex-col justify-between gap-4 rounded-xl border border-border bg-white p-4 font-semibold text-accent-teal shadow-lg shadow-muted_foreground  sm:flex-row md:mx-auto md:w-[80%] md:px-16 xl:w-[50%]'>
+            <div
+                ref={ref}
+                className='mx-4 flex flex-col justify-between gap-4 rounded-xl border border-border bg-white p-4 font-semibold text-accent-teal shadow-lg shadow-muted_foreground  sm:flex-row md:mx-auto md:w-[80%] md:px-16 xl:w-[50%]'
+            >
                 <div className='flex flex-col gap-4'>
                     <CountUp
-                        start={0}
+                        start={startCount}
                         end={313}
+                        duration={3}
+                        startOnMount={true}
+                        redraw
                         style={{
                             letterSpacing: '8px',
                             color: 'transparent',
@@ -17,12 +37,15 @@ const Countup = () => {
                         }}
                         className='text-7xl md-h[80px]'
                     />
+                    
                     <span style={{ color: 'teal' }}>kilometers of code written</span>
                 </div>
                 <div className='flex flex-col gap-4'>
                     <CountUp
-                        start={0}
+                        start={startCount}
                         end={210}
+                        duration={3}
+                        redraw
                         style={{
                             letterSpacing: '8px',
                             color: 'transparent',
@@ -35,7 +58,7 @@ const Countup = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Countup
+export default Countup;
