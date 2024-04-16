6f3e64080886b5ed658cededeb21cc622d3e5206
Sanij <Sanizsanij@gmail.com>
2024-04-16 15:49:23 +0545

    fix[footer]:footer completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import metalogo from "../images/metalogo.png"
-import { MapPin, Phone, Mail } from 'lucide-react';
+import { MapPin, Phone, Mail, Facebook, Instagram, Linkedin } from 'lucide-react';
 
 const Footer = () => {
-    const company = [
-        {
-            title: 'Company', child: ['a', 'b']
-        }
-    ]
     return (
-        <footer className='z-50 w-full bg-primary px-4 pb-8 pt-24 text-white md:px-16 '>
+        <footer className='  relative w-full bg-primary px-4 pb-8 pt-24 text-white md:px-16 '>
             <div className='flex justify-between'>
                 <div className='flex lg:flex-row gap-4 lg:gap-2' >
                     <div>
@@ -20,7 +15,7 @@ const Footer = () => {
                     <div>
                         <div className="tracking-widest font-exo font-bold text-3xl">MetaLogic</div>
                         <div className='text-sm mb-4'>Software Private Limited</div>
-                        
+
                         <ul className='flex flex-col gap-2 text-sm'>
                             <li className='flex gap-2'><MapPin className='w-4 h-4' />Saptakhel, Lalitpur (Head office)</li>
                             <li className='flex gap-2'><Phone className='w-4 h-4' />+ 977 9851353599</li>
@@ -28,19 +23,49 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
-                <div>
-                    {company.map((item, index) => (
-                         <ul key={index}>
-                         <li>{item.title}</li>
-                         <li>{item.child}</li>
-                         {/* Render child elements if needed */}
-                     </ul>
-                    ))}
-
+                <div className='flex flex-col gap-y-8 md:mx-auto md:flex-row md:gap-8 lg:gap-16 xl:mx-0'>
+                    <div>
+                        <h3 className="mb-2 font-semibold">Company</h3>
+                        <ul className="flex flex-col gap-2 text-xs">
+                            <li><a href="/contact">Feedback</a></li>
+                            <li><a href="/contact/partner">Partnership</a></li>
+                            <li><a href="#">Terms and Conditions</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="mb-2 font-semibold">Services</h3>
+                        <ul className="flex flex-col gap-2 text-xs">
+                            <li><a href="/services">Custom Software Development</a></li>
+                            <li><a href="/services">Web Development</a></li>
+                            <li><a href="/services">Mobile App Development</a></li>
+                            <li><a href="/services">Cloud Computing Services</a></li>
+                            <li><a href="/services">Quality Assurance and Testing</a></li>
+                            <li><a href="/services">UI/UX Designing</a></li>
+                            <li><a href="/services">Maintenance and Support</a></li>
+                            <li><a href="/services">Dev Ops</a></li>
+                            <li><a href="/services">Blockchain Solutions</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="mb-2 font-semibold">Join</h3>
+                        <ul className="flex flex-col gap-2 text-xs">
+                            <li><a href="/career">Careers at MetaLogic</a></li>
+                            <li><a href="/career">Internships</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="mb-2 font-semibold">Join Us On Social Medias</h3>
+                        <ul className="flex  gap-4 text-sm">
+                            <li className='rounded-full border-2 p-2'><a href="/career"><Phone /></a></li>
+                            <li className='rounded-full border-2 p-2'><a href="/career"><Facebook /></a></li>
+                            <li className='rounded-full border-2 p-2'><a href="/career"><Instagram /></a></li>
+                            <li className='rounded-full border-2 p-2'><a href="/career"><Linkedin /></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className="mt-8 flex flex-col gap-y-2 text-sm md:flex-row">
-                © Copyright 2024 Metalogic. All rights reserved.
+                © Copyright 2024 MetaLogic. All rights reserved.
             </div>
         </footer>
     )
