81e1532611cffcde46a13989d073d7d8244498fb
Sanij <Sanizsanij@gmail.com>
2024-04-16 15:48:49 +0545

    feat[services]:our services part of home page

--- src/components/OurServices/Servicecard.tsx
+++ src/components/OurServices/Servicecard.tsx
@@ -1,10 +1,10 @@
 import { MonitorSmartphone } from 'lucide-react'
 import React from 'react'
 
-const Servicecard = ({ img, header, text, color }: { img: any, header: any, text: string, color: boolean }) => {
+const Servicecard = ({ img, header, text, backtext, color }: { img: any, header: any, text: string, backtext: string, color: boolean }) => {
     return (
-        <>
-            <div className={`hover:cursor-pointer card scale-up-top hover:flip-vertical-fwd w-52 border border-secondary-800 rounded-xl  ${color ? 'bg-gradient-to-r from-[#FF5d5d] to-[#DD2476] text-white' : ''}`}>
+        <div className='maincard w-52'>
+            <div className={`card shadow-xl scale-up-top hover:flip-vertical-fwd w-52 border border-secondary-800 rounded-xl  ${color ? 'bg-gradient-to-r from-[#FF5d5d] to-[#DD2476] text-white' : ''}`}>
                 <div className='thefront flex flex-col py-4 gap-4 items-center justify-center'>
                     <div className=' rounded-full text-white bg-secondary-800 p-8'>
                         {img}
@@ -14,11 +14,12 @@ const Servicecard = ({ img, header, text, color }: { img: any, header: any, text
                         <p className="px-4 text-sm">{text}</p>
                     </div>
                 </div>
-                <div className="theback items-center justify-center">
-                    Hiiiiiiiiiiii
+                <div className={`theback items-center justify-center ${color ? 'bg-white text-black' : 'bg-gradient-to-r from-[#DD2476] to-[#FF5d5d] text-white'} absolute top-0 w-full h-full rounded-xl flex flex-col gap-4  text-center`}>
+                    <p className='p-2'>{backtext}</p>
+                    <span className='underline underline-offset-2 text-blue-200'>See More</span>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
